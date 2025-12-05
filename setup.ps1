17fbbb2adebed3b43f93bbaf6e79bce2814a840d
Dung Dinh <dungdinhmanh0209@gmail.com>
2025-12-05 23:20:08 +0700

    forgot to update username

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest "https://raw.githubusercontent.com/YourUsername/windoze-auto/main/packages.json" -OutFile packages.json
+Invoke-WebRequest "https://raw.githubusercontent.com/dungdinhmanh/windoze-auto/main/packages.json" -OutFile packages.json
 winget import -i packages.json --accept-source-agreements --accept-package-agreements
 Remove-Item -Path packages.json
