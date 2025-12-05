161957b70078343a29044a28f1e8d6356d942a95
Dung Dinh <dungdinhmanh0209@gmail.com>
2025-12-06 00:29:08 +0700

    update install file

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest "https://raw.githubusercontent.com/dungdinhmanh/windoze-auto/main/packages.json" -OutFile packages.json
-winget import -i packages.json --accept-source-agreements --accept-package-agreements
+winget import -i packages.json
 Remove-Item -Path packages.json
