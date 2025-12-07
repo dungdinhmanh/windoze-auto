3212602fe6303f5812554ee9713f4ef9c75be437
Dung Dinh <dungdinhmanh0209@gmail.com>
2025-12-07 13:36:36 +0700

    refactor: reorganize variable definitions in setup.ps1 for clarity and consistency

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,18 @@
 $gh_raw = 'https://raw.githubusercontent.com/dungdinhmanh/windoze-auto/main/'
-$Packages = "${gh_raw}config/packages.json"
+$notepad_catpuccin = 'https://raw.githubusercontent.com/catppuccin/notepad-plus-plus/main/themes/catppuccin-mocha.xml'
 $Launchers = "${gh_raw}install_script/install-launcher.ps1"
 $PwshProfile = "${gh_raw}config/Microsoft.PowerShell_profile.ps1"
+$Font = "${gh_raw}install_script/install-font.ps1"
+$Packages = "${gh_raw}config/packages.json"
 $starship = "${gh_raw}config/starship.toml"
 $terminal = "${gh_raw}config/settings.json"
-$notepad_catpuccin = 'https://raw.githubusercontent.com/catppuccin/notepad-plus-plus/main/themes/catppuccin-mocha.xml'
-$font = "${gh_raw}install_script/install-font.ps1"
 
 Invoke-WebRequest $Packages -OutFile packages.json
 winget import -i packages.json
 Remove-Item -Path packages.json
 
 Invoke-RestMethod $Launchers | Invoke-Expression
-
-Invoke-RestMethod $font | Invoke-Expression
+Invoke-RestMethod $Font | Invoke-Expression
 
 Invoke-RestMethod $PwshProfile -OutFile "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 Invoke-RestMethod $starship -OutFile "$HOME\starship.toml"
