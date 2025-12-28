29f8938700f1f7735401a24f8a28d23e071b2f86
Dung Dinh <dungdinhmanh0209@gmail.com>
2025-12-28 22:12:04 +0700

    remove komorebi from path

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 Import-Module -Name Terminal-Icons
 $ENV:STARSHIP_CONFIG = "$HOME\starship.toml"
 Invoke-Expression (&starship init powershell)
-$Env:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
+#$Env:KOMOREBI_CONFIG_HOME = "$HOME\.config\komorebi"
